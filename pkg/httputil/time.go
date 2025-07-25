b3c851ee60f1e7de3860da1c47a07877aa6bc840
Sergey Antonov <lenktongm@gmail.com>
2025-07-25 15:09:53 +0300

    feat: add json and postgres marshalling/unmarshalling to Time

--- pkg/httputil/time.go
+++ pkg/httputil/time.go
@@ -1,7 +1,40 @@
 package httputil
 
-import "time"
+import (
+	"database/sql/driver"
+	"fmt"
+	"log"
+	"time"
+)
 
 type Time struct {
 	time.Time
+}
+
+// could i use a pointer??
+func (t Time) Value() (driver.Value, error) {
+	return t.Time, nil
+}
+
+func (t *Time) Scan(src any) error {
+	switch src := src.(type) {
+	case time.Time:
+		t.Time = src
+		return nil
+	default:
+		err := fmt.Errorf("error: cannot handle type: %T", src)
+		// TODO: prettify log message
+		log.Println(err)
+		return err
+	}
+}
+
+func (t *Time) UnmarshalJSON(src []byte) error {
+	parsedTime, err := time.Parse("01-2006", string(src))
+	t.Time = parsedTime
+	return err
+}
+
+func (t Time) MarshalJSON() ([]byte, error) {
+	return fmt.Appendf(nil, "\"%s\"", t.Format("01-2006")), nil
 }
