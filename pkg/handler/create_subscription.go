8dc82f5a328cb5a2fdee45091bbd4ae862bafbf3
Sergey Antonov <lenktongm@gmail.com>
2025-07-30 01:16:29 +0300

    feat: use request body while processing POST /subscriptions

--- pkg/handler/create_subscription.go
+++ pkg/handler/create_subscription.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/jackc/pgx/v5"
@@ -16,11 +18,17 @@ type CreateSubscription struct {
 // ServeHTTP implements http.Handler.
 func (h *CreateSubscription) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	sub := &subscription.Subscription{}
-	uuid := httputil.NewUUID()
-	sub.UserID = uuid
+	err := json.NewDecoder(r.Body).Decode(&sub)
+	if err != nil {
+		httputil.WriteErrorJSON(w, http.StatusUnprocessableEntity, "malformed request body")
+		log.Printf("Error: CreateSubscription#ServeHTTP:Decode: %v\n", err)
+		return
+	}
+
 	h.DB.Exec(
 		context.Background(),
-		"INSERT INTO subscriptions (service_name, price, user_id, start_date, end_date) VALUES ($1, $2, $3, $4, $5)",
+		`INSERT INTO subscriptions (service_name, price, user_id, start_date, end_date)
+		VALUES ($1, $2, $3, $4, $5)`,
 		sub.ServiceName,
 		sub.Price,
 		sub.UserID,
